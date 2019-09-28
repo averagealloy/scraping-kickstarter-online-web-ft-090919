118acfd948cc12d686e525afa69097df5588232e
Mike McTighe <m.mctighe10@gmail.com>
2019-09-28 15:58:25 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,10 +8,5 @@ require "pry"
 # description: project.css("p.bbcard_blurb").text
 # location: project.css("ul.project-meta span.location-name").text
 # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
-def create_project_hash
-  # write your code here
-  html = File.read('fixtures/kickstarter.html')
-  kickstarter = Nokogiri::HTML(html)
-  binding.pry
-end
+
 create_project_hash
